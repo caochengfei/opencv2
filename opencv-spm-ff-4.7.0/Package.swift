8a2312d052a1c6e570ce0d8e3c740f6d9e0b8d35
cofey <cchengfei@outlook.com>
2024-09-09 14:20:03 +0800

    add: 添加文件

---
+++ opencv-spm-ff-4.7.0/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "4.7.0"
+let checksum = "7e578f7734ff25796acc2847163c86c857a11351a2f99b220d11189639786ee9"
+
+let package = Package(
+    name: "OpenCV",
+    platforms: [
+        .macOS(.v10_13), .iOS(.v12), .macCatalyst(.v13)
+    ],
+    products: [
+        .library(
+            name: "OpenCV",
+            targets: ["opencv2", "opencv2-dependencies"]),
+    ],
+    targets: [
+        .binaryTarget(name: "opencv2",
+                      url: "https://github.com/yeatse/opencv-spm/releases/download/\(version)/opencv2.xcframework.zip",
+                      checksum: checksum),
+        .target(
+            name: "opencv2-dependencies",
+            linkerSettings: [
+                .linkedFramework("AVFoundation"),
+                .linkedFramework("CoreImage"),
+                .linkedFramework("CoreMedia"),
+                .linkedFramework("CoreVideo", .when(platforms: [.iOS, .visionOS])),
+                .linkedFramework("Accelerate", .when(platforms: [.iOS, .macOS, .visionOS])),
+                .linkedFramework("OpenCL", .when(platforms: [.macOS])),
+                .linkedLibrary("c++")
+            ]
+        )
+    ]
+)
